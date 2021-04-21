a6c4bba4a08a9f5a1db4bfe045911ab7ecdbd25e
Daniely Xavier <47436403+daniellybx@users.noreply.github.com>
2021-04-21 11:38:29 -0300

    Update 06_modelo_linear.R

--- 06_modelo_linear.R
+++ 06_modelo_linear.R
@@ -22,7 +22,7 @@ model = lm(data = dados_fn, tx_icsab ~ cobertura_esf+gini+idhm+menores_5+maiores
 summary(model)
 step(model, direction = "both", scale = 618.1) #usando o residual standart error para scale
 
-model_fn = lm(formula = tx_icsab ~ cobertura_esf+gini+idhm+menores_5+maiores_60, data = dados_fn[1:5000,])
+model_fn = lm(formula = tx_icsab ~ cobertura_esf+idhm+menores_5+maiores_60, data = dados_fn[1:5000,])
 summary(model_fn)
 
 #plotando o modelo 
@@ -30,7 +30,7 @@ par(mfrow= c(2,2))
 plot(model_fn, which = c(1:4), pch = 20)
 
 #teste shapiro-wilk para normalidade dos resíduos
-shapiro.test(model_fn$residuals)
+sf.test(model_fn$residuals)
 
 # coeficientes do modelo
 model_fn$coefficients
